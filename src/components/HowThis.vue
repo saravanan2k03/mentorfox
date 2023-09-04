7e6da2a7d4e02cd82324a9f5f2af12dda907c31e
saravanan2k03 <sarothehacker@gmail.com>
2023-09-04 06:59:11 +0530

    add

---
+++ src/components/HowThis.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex">
+        <div class="flex flex-col">
+
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "HowThis",
+};
+</script>
